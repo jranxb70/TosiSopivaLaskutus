f49a5a65fdef811d0a5621db7c7a2cbb812bc256
Jussi Rantapere <jussi.rantapere@hotmail.com>
2024-02-13 16:36:32 +0200

    The code used in testing of the new list structure is removed.

--- DatabaseEngine/engine/SqlErrorUtil.c
+++ DatabaseEngine/engine/SqlErrorUtil.c
@@ -18,18 +18,7 @@ void SQLErrorUtil(_In_ SQLRETURN retcode, _In_ SQLHSTMT hstmt, _Out_ node_t** li
     {
         while (SQLError(SQL_NULL_HENV, SQL_NULL_HDBC, hstmt, sqlstate, &native_error, message, sizeof(message), &message_len) != SQL_NO_DATA) {
             printf("SQLSTATE: %s\nNative Error: %ld\nMessage: %s\n", sqlstate, (long)native_error, message);
-            //SQLERRORDETAILS* err_details = (SQLERRORDETAILS*) malloc(2048);
-            //strcpy_s(err_details->sqlstate, 1024, sqlstate);
-            //err_details->native_error = native_error;
-            //err_details->message_len = message_len;
-            //strcpy_s(err_details->message, SQL_MAX_MESSAGE_LENGTH, message);
-
-            //Append(listItems, err_details);
-        }
-
-        for (int i = 0; i < 5; i++)
-        {
-            SQLERRORDETAILS* err_details = (SQLERRORDETAILS*)malloc(2048);
+            SQLERRORDETAILS* err_details = (SQLERRORDETAILS*) malloc(2048);
             strcpy_s(err_details->sqlstate, 1024, sqlstate);
             err_details->native_error = native_error;
             err_details->message_len = message_len;
